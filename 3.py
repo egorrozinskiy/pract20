b70884f4558f0c1acb8c1524e5cf0f8616b9db66
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-03-17 02:24:52 +0400

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+def graf():
+    N = int(input())
+    G = {}
+    for i in range(int(input())):
+        a, b = input().split()
+        if a not in G:
+            G[a] = {b}
+        else:
+            G[a].add(b)
+        if b not in G:
+            G[b] = {a}
+        else:
+            G[b].add(a)
+    return G            
+def bsf(G, P, start, fired):
+    queue = [start]
+    fired.add(start)
+    while len(queue) != 0:
+        current = queue.pop(0)
+        for neighbour in G[current]:
+            if neighbour not in fired:
+                P.add_edge(current, neighbour)
+                fired.add(neighbour)
+                queue.append(neighbour)
+P = nx.Graph()
+G = graf()
+fired = set()
+bsf(G, P, '0', fired)
+nx.draw(P)
+plt.savefig("simple_path.png") # save as png
+plt.show() # display
