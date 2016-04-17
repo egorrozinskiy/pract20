5082da0febd85622077323421445ef1aa1e11595
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-04-17 15:29:21 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,33 +1,32 @@
 def graph_input():
-    N = int(input('Количество вершин в графе: '))
-    weight = [[float('+inf')]*N for j in range(N)]
-    for i in range(N):
-        weight[i][i] = 0
-    M = int(input('Количество ребер в графе: '))
-    for i in range(M):
-        a, b, w = input().split()
-        a, b, w = int(a), int(b), float(w)
-        weight[a][b] = w
-        weight[b][a] = w
-    return weight
-
-def print_matrix(A):
-    for i in range(len(A)):
-        print(*A[i], sep='\t')
-    print()
-
-from copy import deepcopy
-
-def floyd(weight):
-    N = len(weight)
-    F = deepcopy(weight)
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                F[i][j] = min(F[i][j], F[i][k] + F[k][j])
-    return F
-
-G = graph_input()
-print_matrix(G)
-F = floyd(G)
-print_matrix(F)
+    G={}
+    M=int(input())
+    for i in range (M):
+        a,b,weight = input().split()
+        weight=float(weight)
+        if a not in G:
+            G[a]={b:weight}
+        else:
+            G[a][b]=weight
+        if b not in G:
+            G[b]={a:weight}
+        else:
+            G[b][a]=weight
+    return G
+def dejkstra(G,start):
+    shortest_path={vertex:float('+inf') for vertex in G}
+    shortest_path[start] = 0
+    queue = [start]
+    while queue:
+        current=queue.pop(0)
+        for neighbour in G[current]:
+            offering_shortest_path = shortest_path[current]+(G[current][neighbour])
+            if offering_shortest_path < shortest_path[neighbour]:
+                shortest_path[neighbour]=offering_shortest_path
+                queue.append(neighbour)
+    return shortest_path
+G=graph_input()
+start=input()
+shortest_path=dejkstra(G, start)
+for vertex in G:
+    print(vertex,shortest_path[vertex])
